7dd4de4aa289a8750a3e23c3cb4b2638d82ff987
jacob24068 <30918688+jacob24068@users.noreply.github.com>
2018-09-23 17:56:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ DiscordClient = new Discord.Client()
 DiscordClient.login(process.env.BOT_TOKEN);
 
 //pgSQL Initialization
-const { Client } = require('pg');
-const pgClient = new Client({connectionString: process.env.DATABASE_URL, ssl: true});
+const {
+    Client
+} = require('pg');
+const pgClient = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
 
 //Request
 const request = require('request')
@@ -25,7 +30,12 @@ let saveData = {}
 let guild, notifications, log, welcome, role
 
 DiscordClient.on("ready", () => {
-    DiscordClient.user.setPresence({ game: { name: `over Brice's server`, type: 3 } });
+    DiscordClient.user.setPresence({
+        game: {
+            name: `over Brice's server`,
+            type: 3
+        }
+    });
     guild = DiscordClient.guilds.get(`459074666137649162`)
     notifications = DiscordClient.channels.get(`459078238283497472`)
     log = DiscordClient.channels.get(`459077897525788692`)
@@ -49,13 +59,13 @@ DiscordClient.on("message", message => {
         saveData[message.author.id] = 0
     }
     saveData[message.author.id] = saveData[message.author.id] + 1
-    if (saveData[message.author.id] > 2000 && !message.member.roles.some(r=>["Regular"].includes(r.name))){
+    if (saveData[message.author.id] > 2000 && !message.member.roles.some(r => ["Regular"].includes(r.name))) {
         message.member.addRole(message.guild.roles.find('name', 'Regular'))
     }
     if (!message.guild) return
     if (message.author.bot) return
     //if (config.responses[message.content.toLowerCase()]) return message.channel.send(config.responses[message.content.toLowerCase()])
-    if (message.content.substr(0,prefix.length) != prefix) return
+    if (message.content.substr(0, prefix.length) != prefix) return
     const args = message.content.slice(prefix.length).trim().split(/ +/g)
     const command = args.shift().toLowerCase()
     if (aliases[command]) return aliases[command](message, args)
@@ -77,29 +87,33 @@ const aliases = {
 
 }
 
-function isElevated(guildMember){
-    return guildMember.roles.some(r=>["Bot", "Moderator", "Adminstrator", "Brice"].includes(r.name))
+function isElevated(guildMember) {
+    return guildMember.roles.some(r => ["Bot", "Moderator", "Adminstrator", "Brice"].includes(r.name))
 }
 
-function randomColor(){
-    return Number("0x"+Math.floor(Math.random()*16777215).toString(16))
+function randomColor() {
+    return Number("0x" + Math.floor(Math.random() * 16777215).toString(16))
 }
 
 const save = function() {
     pgClient.query(`DELETE FROM userdata`, null, (err, res) => {
-      if (err) {console.log(err.stack)}
+        if (err) {
+            console.log(err.stack)
+        }
     })
     pgClient.query(`INSERT INTO userdata(info) VALUES($1)`, [JSON.stringify(saveData)], (err, res) => {
-      if (err) {console.log(err.stack)}
+        if (err) {
+            console.log(err.stack)
+        }
     })
 }
 
-function getUser(message, match){
+function getUser(message, match) {
     message.guild.members.forEach(user => {
         if (user.displayName.toLowerCase().match(match.toLowerCase())) {
             return user
         }
-    });    
+    });
 }
 
 function ordinal_suffix_of(i) {
@@ -117,159 +131,159 @@ function ordinal_suffix_of(i) {
     return i + "th";
 }
 
-function verify(message){
+function verify(message) {
     if (message.channel.id != `459081091240689670`) return
     log.send(`${message.author} has verified at ${Date()}`)
     message.delete()
     welcome.send({
-      "embed": {
-        "title": `${message.member.displayName} joined.`,        
-        "description": `${ordinal_suffix_of(message.guild.members.length)} member.`,
-        "color": 1542474,
-        "fields": [{
-            "name": "Joined BBG's Server",
-            "value": (new Date()).toLocaleString()
-        },
-        {
-            "name": "Joined Discord",
-            "value": message.author.createdAt.toLocaleString()
+        "embed": {
+            "title": `${message.member.displayName} joined.`,
+            "description": `${ordinal_suffix_of(message.guild.members.length)} member.`,
+            "color": 1542474,
+            "fields": [{
+                    "name": "Joined BBG's Server",
+                    "value": (new Date()).toLocaleString()
+                },
+                {
+                    "name": "Joined Discord",
+                    "value": message.author.createdAt.toLocaleString()
+                }
+            ],
+            "thumbnail": {
+                "url": message.author.displayAvatarURL
+            }
         }
-    ],
-        "thumbnail": {
-          "url": message.author.displayAvatarURL
-        }
-      }
     })
     message.member.addRole(message.guild.roles.find("name", "Fan")).catch(console.error);
 }
 
-function points(message, args){
+function points(message, args) {
     if (message.mentions.members.first()) {
         message.channel.send({
             "embed": {
-              "title": `${message.mentions.members.first().displayName}, you have ${saveData[message.mentions.members.first().id]} points.`,        
-              "color": randomColor()
+                "title": `${message.mentions.members.first().displayName}, you have ${saveData[message.mentions.members.first().id]} points.`,
+                "color": randomColor()
             }
-          })
-    }else if (args[0]) {
+        })
+    } else if (args[0]) {
         let member = getUser(message, args[0])
         if (member) return message.channel.send({
             "embed": {
-              "title": `${user.displayName} has ${saveData[user.id]} points.`,        
-              "color": randomColor()
+                "title": `${user.displayName} has ${saveData[user.id]} points.`,
+                "color": randomColor()
             }
-          })
-    }
-    else{
+        })
+    } else {
         message.channel.send({
             "embed": {
-              "title": `${message.member.displayName}, you have ${saveData[message.author.id]} points.`,        
-              "color": randomColor()
+                "title": `${message.member.displayName}, you have ${saveData[message.author.id]} points.`,
+                "color": randomColor()
             }
-          })
+        })
     }
 }
 
-function forceSave(message){
+function forceSave(message) {
     if (!isElevated(message.member)) return
     save()
     message.channel.send({
         "embed": {
-          "title": `Finished.`,        
-          "color": 1542474
+            "title": `Finished.`,
+            "color": 1542474
         }
-      })
+    })
 }
 
-function setPoints(message, args){
+function setPoints(message, args) {
     if (!isElevated(message.member)) return
     let user
     if (message.mentions.members.first()) {
         user = message.mentions.members.first()
-    }else if (args[1]) {
+    } else if (args[1]) {
         user = getUser(message, args[0])
     }
     if (!user) return
     saveData[user.id] = Number(args[1])
     message.channel.send({
         "embed": {
-          "title": `Finished.`,        
-          "color": 1542474
+            "title": `Finished.`,
+            "color": 1542474
         }
-      })
+    })
 }
 
 let streaming = {}
 
-function streamnotifications(message){
+function streamnotifications(message) {
     if (message.member.roles.some(r => ["StreamNotifications"].includes(r.name))) {
         message.member.removeRole(message.guild.roles.find('name', 'StreamNotifications'))
         message.channel.send({
             "embed": {
-              "title": 'You will no longer be mentioned when Brice streams.',        
-              "color": 12451840
+                "title": 'You will no longer be mentioned when Brice streams.',
+                "color": 12451840
             }
         })
-    }else {
+    } else {
         message.member.addRole(message.guild.roles.find('name', 'StreamNotifications'))
         message.channel.send({
             "embed": {
-              "title": 'You will now be mentioned when Brice streams.',        
-              "color": 12451840
+                "title": 'You will now be mentioned when Brice streams.',
+                "color": 12451840
             }
         })
     }
 }
 
-function leaderboard(message, args){
+function leaderboard(message, args) {
     const keys = Object.keys(saveData);
     let newT = {}
-    for(let i=0;i<keys.length;i++){
-      let key = keys[i];
-      newT[saveData[key]] = key
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        newT[saveData[key]] = key
     }
     var arr = [];
     for (var prop in newT) {
         arr.push(newT[prop]);
     }
     const Length = arr.length - 1
-    for(var i = 0; i <= 9; i++) {
-    if (!message.guild.members.get(arr[Length-i])) {
-     delete saveData[arr[Length-i]]
-      }
+    for (var i = 0; i <= 9; i++) {
+        if (!message.guild.members.get(arr[Length - i])) {
+            delete saveData[arr[Length - i]]
+        }
     }
     const member = message.mentions.members.first()
     let number = args.slice(0).join(' ');
     if (member) {
-      for (var i = 0, row; row = arr[i]; i++) {
-        if (row == member.id) return message.channel.send(`${member.displayName} is in ${ordinal_suffix_of(arr.length-i)} place, with ${saveData[member.id].toLocaleString()} points.`)
-      }
-    }else if (number) {
-      let b = arr.length
-      if (arr[b - number]) {
-        let mem = message.guild.members.get(arr[b-number])
-        message.channel.send(`${mem.displayName} is in ${ordinal_suffix_of(number)} place with ${saveData[mem.id].toLocaleString()} points.`)
-      }else return message.channel.send(`There is no person in ${ordinal_suffix_of(number)} place.`)
-    }else{
+        for (var i = 0, row; row = arr[i]; i++) {
+            if (row == member.id) return message.channel.send(`${member.displayName} is in ${ordinal_suffix_of(arr.length-i)} place, with ${saveData[member.id].toLocaleString()} points.`)
+        }
+    } else if (number) {
+        let b = arr.length
+        if (arr[b - number]) {
+            let mem = message.guild.members.get(arr[b - number])
+            message.channel.send(`${mem.displayName} is in ${ordinal_suffix_of(number)} place with ${saveData[mem.id].toLocaleString()} points.`)
+        } else return message.channel.send(`There is no person in ${ordinal_suffix_of(number)} place.`)
+    } else {
         let fields = []
-        for (let i = 0; i <= 9; i++){
-            if (arr[Length-i]){
+        for (let i = 0; i <= 9; i++) {
+            if (arr[Length - i]) {
                 fields.push({
                     "name": `${i+1} - ${String(message.guild.members.get(arr[Length-i]).displayName)}`,
                     "value": `${(saveData[arr[Length-i]]).toLocaleString()} points ${arr[Length-i-1] && (`(Ahead ${(saveData[arr[Length-i]] - saveData[arr[Length-i-1]]).toLocaleString()} points)`) || ""}`
-                  })
+                })
             }
         }
-    message.channel.send({
-      "embed": {
-        "title": "BBG Leaderboard",
-        "color": randomColor(),
-        "fields": fields
-      }
-    })}
+        message.channel.send({
+            "embed": {
+                "title": "BBG Leaderboard",
+                "color": randomColor(),
+                "fields": fields
+            }
+        })
+    }
 }
 
-function poll(message){
+function poll(message) {
     if (!isElevated(message.member)) return
     let split = message.content.split('"')
     let items = []
@@ -278,22 +292,25 @@ function poll(message){
     }
     let fields = []
     message.delete()
-    items.forEach(function(text, index){
-      if (!index == 0) {
-        fields.push({"name": "Option " + alphabet[index-1], "value": text.replace(`"`, "").replace(`"`, "")})
-      }
+    items.forEach(function(text, index) {
+        if (!index == 0) {
+            fields.push({
+                "name": "Option " + alphabet[index - 1],
+                "value": text.replace(`"`, "").replace(`"`, "")
+            })
+        }
     })
     message.channel.send({
-      "embed": {
-          "title": `${message.member.displayName} has started a poll.`,
-          "description": `${items[1]}`,
-          "color": randomColor(),
-          "fields": fields
-      }
-    }).then(function(message){
-      fields.forEach(function(a, index){
-        message.react(alphabet[index])
-      })
+        "embed": {
+            "title": `${message.member.displayName} has started a poll.`,
+            "description": `${items[1]}`,
+            "color": randomColor(),
+            "fields": fields
+        }
+    }).then(function(message) {
+        fields.forEach(function(a, index) {
+            message.react(alphabet[index])
+        })
     })
 }
 //Stream Notifications
@@ -312,26 +329,25 @@ DiscordClient.on("presenceUpdate", (old, user) => {
             if (!body) return
             let gamename = String(game.name)
             body = JSON.parse(body)
-            role.setMentionable(true).then(function(){
-              notifications.send('<@&460105041563615234>')
-              role.setMentionable(false)
-              notifications.send({
-                  "embed": {
-                      "title": `${user.displayName} has started streaming!`,
-                      "description": `You can watch the stream [here](${game.url})`,
-                      "color": Number("0x"+Math.floor(Math.random()*16777215).toString(16)),
-                      "footer": {
-                          "text": "*Information based on twitch and user settings."
-                      },
-                      "thumbnail": {
-                          "url": body["logo"]
-                      },
-                      "fields": [{
-                          "name": `Streaming "${game.name}"`,
-                          "value": `Playing ${body["game"]}`
-                      }]
-                  }
-              })
+            await role.setMentionable(true)
+            await notifications.send('<@&460105041563615234>')
+            await role.setMentionable(false)
+            notifications.send({
+                "embed": {
+                    "title": `${user.displayName} has started streaming!`,
+                    "description": `You can watch the stream [here](${game.url})`,
+                    "color": Number("0x" + Math.floor(Math.random() * 16777215).toString(16)),
+                    "footer": {
+                        "text": "*Information based on twitch and user settings."
+                    },
+                    "thumbnail": {
+                        "url": body["logo"]
+                    },
+                    "fields": [{
+                        "name": `Streaming "${game.name}"`,
+                        "value": `Playing ${body["game"]}`
+                    }]
+                }
             })
         }
     })
@@ -343,15 +359,15 @@ DiscordClient.on('guildMemberRemove', member => {
     log.send(`${member} has left at ${new Date()}`)
 });
 DiscordClient.on('messageDelete', message => {
-  log.send({
-    "embed": {
-      "title": `Message deleted`,
-      "color": Number("0x"+Math.floor(Math.random()*16777215).toString(16)),
-      "timestamp": new Date(),
-      "fields": [{
-        "name": `Message posted by ${message.author} in ${message.channel} deleted.`,
-        "value": message.content
-      }]
-    }
-  })
+    log.send({
+        "embed": {
+            "title": `Message deleted`,
+            "color": Number("0x" + Math.floor(Math.random() * 16777215).toString(16)),
+            "timestamp": new Date(),
+            "fields": [{
+                "name": `Message posted by ${message.author} in ${message.channel} deleted.`,
+                "value": message.content
+            }]
+        }
+    })
 })
